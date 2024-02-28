c88626273233f2836a344a7eef166c6f775c9a32
Fernando005-SSF <sadeepfernando12345@gmail.com>
2024-02-28 14:18:59 +0530

    sign in part

--- src/Componenets/Login.jsx
+++ src/Componenets/Login.jsx
@@ -3,8 +3,40 @@ import './Login.css'
 
 const Login = () => {
   return (
-    <div>
-      <h1>HI</h1>
+    <div class="container">
+
+      <div className="header">
+        <h1>Sign Up</h1>
+      </div>
+
+      <div className="fields">
+
+        <div className="text">
+          <h3>Name</h3>
+          <input type="text" />
+        </div>
+
+        <div className="text">
+          <h3>Email</h3>
+          <input type="email" />
+        </div>
+
+        <div className="text">
+          <h3>Password</h3>
+          <input type="password" />
+        </div>
+
+      </div>
+
+      <div className="buttons">
+
+        <div> <button>sign up</button> </div>
+        <div><button>Log in</button></div>
+
+      </div>
+
+
+
     </div>
   )
 }
